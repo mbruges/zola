9c618bc3b04a8a33f89b3adcfac790533e15f42d
Max Bruges <mclbruges+git@gmail.com>
2024-12-09 08:15:15 +0400

    Dynamic nav styling - really should test how much CPU it uses but it looks good for now.

--- minify.sh
+++ minify.sh
@@ -1,9 +1,16 @@
-cd ~/zola/static
+cd static
+
+
 for file in *.css; do
-    if [[ ! $file == *".min."* ]]; then
+    if [[ ! $file == *".min."* ]] && git diff --name-only HEAD -- "$file" | grep -q "$file"; then
         curl -X POST -s --data-urlencode "input=$(cat $file)" "https://www.toptal.com/developers/cssminifier/api/raw" -o "${file%.css}.min.css"
-        original_size=$(stat -c%s "$file")
-        minified_size=$(stat -c%s "${file%.css}.min.css")
+        if [[ "$(uname)" == "Darwin" ]]; then
+            original_size=$(stat -f%z "$file")
+            minified_size=$(stat -f%z "${file%.css}.min.css")
+        else
+            original_size=$(stat -c%s "$file")
+            minified_size=$(stat -c%s "${file%.css}.min.css")
+        fi
         reduction=$((original_size - minified_size))
         echo "Reduction for $file: $reduction bytes"
     fi

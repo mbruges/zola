734e9cb562e8103e9801c449127fa807aaf85079
Max Bruges <mclbruges+git@gmail.com>
2024-12-09 08:31:22 +0400

    fixed Live Learn websocket - script now lives on index.html

--- static/scripts.js
+++ static/scripts.js
@@ -1,39 +1,3 @@
-export const connectToLiveLesson = () => {
-const iframe = document.querySelector("iframe");
-    const overlay = document.querySelector(".overlay");
-    overlay.addEventListener("click", () => {
-        window.open(iframe.src, "_blank");
-    });
-    var subURL = "wss://socket.maxbruges.com/ws";
-    function checkLiveLesson(url) {
-        webSocket = new WebSocket(url);
-        webSocket.onopen = () => {
-            let now = new Date().toLocaleTimeString();
-        };
-        webSocket.onmessage = (event) => {
-            console.log(event.data);
-            liveURL = event.data;
-            if (liveURL.includes("abort") || liveURL.length == 0) {
-                document.getElementById('learn-button-link').href = '/learn';
-            } else {
-                var preview = document.getElementById("live-preview");
-                var button = document.getElementById("learn-button");
-                preview.style.display = "block";
-                button.innerHTML =
-                    "Join the lesson now! <span id='live-icon'>LIVE 🔴</span>";
-                preview.src = "https://learn.maxbruges.com/" + liveURL;
-                document.getElementById('learn-button-link').href = 'https://learn.maxbruges.com/';
-                button.href = 'https://learn.maxbruges.com/';
-            }
-            let now = new Date().toLocaleTimeString();
-            webSocket.close();
-        };
-    }
-    window.onload = () => {
-        checkLiveLesson(subURL);
-    };
-}
-
 export const toggleNightmode = () => {
   const nightmodeEnabled = document.documentElement.style.getPropertyValue('--t') !== document.documentElement.style.getPropertyValue('--b');
   localStorage.setItem('nightmode', nightmodeEnabled ? 'disabled' : 'enabled');

deba4d540eefe2dedc8e373eba2083bf47d98689
Max Bruges <mclbruges+git@gmail.com>
2024-12-13 12:38:35 +0400

    tweaked minify script to see if it will work

--- .github/scripts/minify.sh
+++ .github/scripts/minify.sh
@@ -1,16 +1,18 @@
+echo "Running minify.sh"
 cd static
 for file in *.css; do
-    if [[ ! $file == *".min."* ]] && git diff --name-only HEAD -- "$file" | grep -q "$file"; then
+    if [[ ! $file == *".min."* ]]; then
+        echo "minifying $file"
         curl -X POST -s --data-urlencode "input=$(cat $file)" "https://www.toptal.com/developers/cssminifier/api/raw" -o "${file%.css}.min.css"
-        if [[ "$(uname)" == "Darwin" ]]; then
-            original_size=$(stat -f%z "$file")
-            minified_size=$(stat -f%z "${file%.css}.min.css")
-        else
-            original_size=$(stat -c%s "$file")
-            minified_size=$(stat -c%s "${file%.css}.min.css")
-        fi
-        reduction=$((original_size - minified_size))
-        echo "Reduction for $file: $reduction bytes"
+        # if [[ "$(uname)" == "Darwin" ]]; then
+        #     original_size=$(stat -f%z "$file")
+        #     minified_size=$(stat -f%z "${file%.css}.min.css")
+        # else
+        #     original_size=$(stat -c%s "$file")
+        #     minified_size=$(stat -c%s "${file%.css}.min.css")
+        # fi
+        # reduction=$((original_size - minified_size))
+        # echo "Reduction for $file: $reduction bytes"
         #Adding minified snippet to homepage
         if [[ $file == *"homepage"* ]]; then
             echo "updating homepage-style snippet"

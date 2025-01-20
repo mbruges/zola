8952d540b91d026619a145030ea5daf98bf10db9
Max Bruges <mclbruges+git@gmail.com>
2025-01-20 09:17:55 +0400

    uploading r+j

--- .github/scripts/get_definitions.sh
+++ .github/scripts/get_definitions.sh
@@ -20,7 +20,9 @@ TEXT=$( echo $TEXT | sed 's|- DEFINITIONS:|\nHere is the current list of defined
 QUERY="You will generate a list of challenging vocabulary and their definitions, to help a student understand the given text. First, read the text; then select specific vocabulary that a 15 year old may need help understanding. Be selective, only picking a few of the advanced and important vocabulary essential for understanding the text - a longer text will need more definitions. Then, generate a list of those words with short definitions (limit to 5 simple words) - consider context in which word is being used. The list must be as a Markdown list, in the following format: - decimate: (v) to destroy, ruin \n  - fastidious: (adj) extremely tidy, precise\n Here is the text: $TEXT\nNow, generate the list of words and definitions. No commentary, only the list $ADDITIONAL"
 
 QUERY="$(echo -n "$QUERY" | jq -sRr @uri)"
+
 OUTPUT=$(curl -s "https://api.mxb.fyi/gpt-mini?query=$QUERY")
+echo $OUTPUT
 
 if [ ${#ADDITIONAL} -gt 4 ]; then
     echo -e "\n- DEFINITIONS:" >> $FILE_PATH
